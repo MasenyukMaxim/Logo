8d92cc1b98d6f7832cfd56d6e8974ca1260f12f5
MasenyukMaxim <maxim.m.2001@yandex.ru>
2023-12-01 18:33:27 +0300

    add media queries for footer

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@
 
 const menuButton = document.querySelector(".button-menu");
 if (menuButton) {
+   const body = document.body;
    const menuBody = document.querySelector(".menu");
    const buttonHeader = document.querySelector(".header__button");
    menuButton.addEventListener("click", function (event) {
       menuButton.classList.toggle("_active");
       menuBody.classList.toggle("_active");
       buttonHeader.classList.toggle("_active");
+      body.classList.toggle("_lock");
    });
 }
 
