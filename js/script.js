aec301e305cca4581d961b2de58146e645b87195
MasenyukMaxim <maxim.m.2001@yandex.ru>
2023-12-02 14:43:09 +0300

    add slider

--- js/script.js
+++ js/script.js
@@ -14,7 +14,80 @@ if (menuButton) {
    });
 }
 
+// слайдер
 
+let offset = 0;
+let width;
+let count;
+const checkupItems = document.querySelector(".check-up__items");
+const numberNow = document.querySelector(".menu-slider__number-now");
+
+function init() {
+   width = checkupItems.offsetWidth;
+
+   if (count) {
+      offset = width * count;
+   }
+
+   rollSlider();
+
+   console.log(width);
+}
+
+function checkNumber() {
+   if (offset === width) {
+      numberNow.innerHTML = "2";
+
+   } else if (offset === width * 2) {
+      numberNow.innerHTML = "3";
+
+   } else if (offset === width * 3) {
+      numberNow.innerHTML = "4";
+   } else {
+      numberNow.innerHTML = "1";
+   }
+}
+
+function rollSlider() {
+   checkupItems.style.left = -offset + "px";
+}
+
+function countNumberSlider() {
+   count = offset / width;
+}
+
+window.addEventListener("resize", init);
+init();
+
+document.querySelector(".menu-slider__right-buttons").addEventListener("click", function () {
+
+   offset = offset + width;
+
+   if (offset > width * 3) {
+      offset = width * 3;
+   }
+
+   rollSlider();
+
+   checkNumber();
+
+   countNumberSlider();
+});
+
+document.querySelector(".menu-slider__left-buttons").addEventListener("click", function () {
+
+   offset = offset - width;
+
+   if (offset < 0) {
+      offset = 0;
+   }
+
+   rollSlider();
+
+   checkNumber();
+
+   countNumberSlider();
+});
 
 
 
